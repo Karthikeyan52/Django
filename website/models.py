047cb05d8e83fef68a4ac4765d614bb74535d5d7
curly <karthiofficial52@gmail.com>
2024-01-07 19:36:05 +0530

    First Project

--- website/models.py
+++ website/models.py
@@ -5,19 +5,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserCreationForm
 
 
-class login(UserCreationForm):
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'password1',
-        ]
-
 
 class Enquiry(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(null=False, blank=False, default='')
-    contact = models.BigIntegerField(max_length=11)
+    contact = models.BigIntegerField()
     subject = models.CharField(max_length=200, default='')
     status = models.CharField(max_length=100, default='')
     comment = models.CharField(max_length=1000, default='')
